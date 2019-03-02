90e8f748f8ce1f385332c068cdac58312f001ed8
FrancisSakwa89 <sakwafrancis89@gmail.com>
2019-03-02 15:36:51 +0300

    modified send/views.py

--- send/views.py
+++ send/views.py
@@ -7,20 +7,20 @@ from django.http import HttpResponse
 
 @csrf_exempt
 def index(request):
-    if request.method == 'POST':
-        session_id = request.POST.get('sessionId')
-        service_code = request.POST.get('serviceCode')
-        phone_number = request.POST.get('phoneNumber')
-        text = request.POST.get('text')
+    # if request.method == 'POST':
+    #     session_id = request.POST.get('sessionId')
+    #     service_code = request.POST.get('serviceCode')
+    #     phone_number = request.POST.get('phoneNumber')
+    #     text = request.POST.get('text')
 
-        response = ""
+    #     response = ""
 
-        if text == "":
-            response = "CON What would you want to check \n"
-            # response .= "1. My Account \n"
-            response += "1. My Phone Number"
+    #     if text == "":
+    #         response = "CON What would you want to check \n"
+    #         # response .= "1. My Account \n"
+    #         response += "1. My Phone Number"
 
-        elif text == "1":
-            response = "END My Phone number is {0}".format(phone_number)
+    #     elif text == "1":
+    #         response = "END My Phone number is {0}".format(phone_number)
 
-        return HttpResponse(response)
+        return render(request, 'index.html')
